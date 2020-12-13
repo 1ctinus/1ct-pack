a650e9699b029c36b408e74ae03665bddab9ddb6
1ctinus <72636637+1ctinus@users.noreply.github.com>
2020-12-13 08:09:28 -0600

    fuck it didnt work

---
+++ extension.js
@@ -0,0 +1,22 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require('vscode');
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+// @ts-ignore
+exports.activate = activate;
+
+// this method is called when your extension is deactivated
+function deactivate() {}
+
+module.exports = {
+	// @ts-ignore
+	activate,
+	deactivate
+}
+// [c:\Users\jtgc0\1ct-pack]: properties `activationEvents` and `main` must both be specified or must both be omitted
