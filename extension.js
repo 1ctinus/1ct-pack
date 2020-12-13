46597ca45dc8d4d606af5b95c762a2a8a85ae5cb
1ctinus <72636637+1ctinus@users.noreply.github.com>
2020-12-13 08:13:29 -0600

    FUCK YOU

--- extension.js
+++ extension.js
@@ -13,8 +13,16 @@ function activate(context) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "1ct-pack" is now active!');
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with  registerCommand
+	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('1ct-pack.helloWorld', function () {
-		vscode.window.showInformationMessage('Hello World from 1ct Pack!');});
+		// The code you place here will be executed every time your command is executed
+
+		// Display a message box to the user
+		vscode.window.showInformationMessage('Hello World from 1ct Pack!');
+	});
 
 	context.subscriptions.push(disposable);
 }
